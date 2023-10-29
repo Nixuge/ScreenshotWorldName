62b4bb18b48bcd913e19dedd0492fa311794a2e1
Nixuge <mail@nixuge.me>
2023-10-29 13:40:48 +0100

    Better mixin + trying to fix build system

--- src/main/java/me/nixuge/screenshotworldname/mixins/util/ScreenShotHelperMixin.java
+++ src/main/java/me/nixuge/screenshotworldname/mixins/util/ScreenShotHelperMixin.java
@@ -1,25 +1,27 @@
 package me.nixuge.screenshotworldname.mixins.util;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ServerData;
-import net.minecraft.client.shader.Framebuffer;
-import net.minecraft.util.IChatComponent;
 import net.minecraft.util.ScreenShotHelper;
 import net.minecraftforge.common.DimensionManager;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ScreenShotHelper.class)
 public class ScreenShotHelperMixin {
-    private static Character[] VALID_CHARS_ARR = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
-    private static List<Character> VALID_CHARS = Arrays.asList(VALID_CHARS_ARR);
+    @Unique
+    private final static Character[] VALID_CHARS_ARR = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'};
+    @Unique
+    private final static List<Character> VALID_CHARS = Arrays.asList(VALID_CHARS_ARR);
 
-    private static String sanitizeText(String text) {
+    @Unique
+    private static String screenshotWorldName$sanitizeText(String text) {
         StringBuilder sb = new StringBuilder();  
         for (char currentChar : text.toLowerCase().toCharArray()) {
             if (VALID_CHARS.contains(currentChar)) {
@@ -29,15 +31,16 @@ public class ScreenShotHelperMixin {
         return sb.toString();
     }
 
-    @Overwrite
-    public static IChatComponent saveScreenshot(File gameDirectory, int width, int height, Framebuffer buffer) {
-        String name = "undefined";
 
+    @ModifyVariable(method = "saveScreenshot(Ljava/io/File;Ljava/lang/String;IILnet/minecraft/client/shader/Framebuffer;)Lnet/minecraft/util/IChatComponent;", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private static String injected(String x) {
+        String name;
         ServerData data = Minecraft.getMinecraft().getCurrentServerData();
+
         if (data != null) {
             name = Minecraft.getMinecraft().getCurrentServerData().serverName;
 
-            if (name == null || name == "")
+            if (name == null || name.equals(""))
                 name = Minecraft.getMinecraft().getCurrentServerData().serverIP.replace(":25565", "");
         } else {
             //// Actual world name
@@ -47,9 +50,9 @@ public class ScreenShotHelperMixin {
             name = DimensionManager.getCurrentSaveRootDirectory().toPath().getFileName().toString();
         }
 
-        name = sanitizeText(name);
+        name = screenshotWorldName$sanitizeText(name);
         name = name + ".png";
 
-        return ScreenShotHelper.saveScreenshot(gameDirectory, name, width, height, buffer);
+        return name;
     }
 }
